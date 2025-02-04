a2e15997dbd6e98eb37e24e902b23681a5d68a49
James <msekelaj@grinnell.edu>
2025-02-04 15:42:29 -0600

    Adding bottom bar navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,6 @@
-import 'react-native-gesture-handler';
 import React from 'react';
-import { SafeAreaView } from 'react-native';
 import AppNavigator from './app/navigation/AppNavigator';
 
 export default function App() {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <AppNavigator />
-    </SafeAreaView>
-  );
+    return <AppNavigator />;
 }
--- app/navigation/AppNavigator.tsx
+++ app/navigation/AppNavigator.tsx
@@ -1,23 +1,46 @@
 // app/navigation/AppNavigator.tsx
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from '../screens/HomeScreen';
 import CreatePulse from '../screens/CreatePulse';
 import EventDetails from '../screens/EventDetails';
 import Profile from '../screens/Profile';
+import { Ionicons } from '@expo/vector-icons';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="CreatePulse" component={CreatePulse} />
-        <Stack.Screen name="EventDetails" component={EventDetails} />
-        <Stack.Screen name="Profile" component={Profile} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            // Assign a default value to avoid "undefined" errors
+            let iconName: keyof typeof Ionicons.glyphMap = 'home';
+
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'CreatePulse') {
+              iconName = 'add-circle';
+            } else if (route.name === 'EventDetails') {
+              iconName = 'calendar';
+            } else if (route.name === 'Profile') {
+              iconName = 'person';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="CreatePulse" component={CreatePulse} />
+        <Tab.Screen name="EventDetails" component={EventDetails} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
